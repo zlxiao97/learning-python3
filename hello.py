d6f360d793c3fd007f93d4638a82a7e258b91311
Tad <zlxiao97@yhaoo.com>
2021-04-09 11:46:24 +0800

    basic syntax laerning

---
+++ hello.py
@@ -0,0 +1,96 @@
+# This is my first comments!
+print('Hello Pyton3!')
+
+# print(type(1))
+
+# print(type(1.3))
+
+# num = 1
+
+# print(num)
+
+# num = num -1
+
+# print(num)
+
+# num += 10
+
+# print(num)
+
+# num = num ** 2
+
+# print(num)
+
+# num = num % 3
+
+# print(num)
+
+# text ="hello hello hello 'Python3'!"
+
+# print(text)
+
+# count = text.count('hello')
+
+# print(count)
+
+# describe = 'I\'m {0}'.format('Tad Xiao')
+
+# print(describe)
+
+# print(type(1))
+
+# print(type(1)==int)
+
+# print(int)
+
+# arr = [1,2,3]
+
+# print(arr)
+
+# print(len(arr))
+
+# arr.append(123)
+
+# print(arr)
+
+# value = arr.pop()
+
+# print(value)
+
+# print(arr)
+
+# print(123 in arr)
+
+# print(1 in arr)
+
+# obj = {
+#     'test':1
+# }
+
+# print(obj)
+
+# print(obj['test'])
+
+# print(obj.keys())
+
+# print(obj.values())
+
+# def handleKeyDown(e):
+#     if e['key'] == 'ArrowLeft':
+#         return 'left'
+#     elif e['key'] == 'ArrowRight':
+#         return 'right'
+#     else:
+#         return 'unknown'
+
+# i = 0
+# while i < 2:
+#     print(handleKeyDown({'key':'ArrowLeft'}))
+#     print(handleKeyDown({'key':'ArrowRight'}))
+#     print(handleKeyDown({'key':'ArrowUp'}))
+#     i += 1
+
+colors = ['red','green']
+
+for color in colors:
+    print('I hate {0}!'.format(color))
